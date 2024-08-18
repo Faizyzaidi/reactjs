48c5ebb57596858b78988eb14ad22e9f23e61578
Faizyzaidi <faizyzaidi80@gmail.com>
2024-08-18 11:04:16 +0000

    ch

--- reactjs/src/component/Navbar.jsx
+++ reactjs/src/component/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import './Navbar.css'
-function Navbar() {
+function Navbar(props) {
   return (
     <div className ="navbar">
       <ul>
+        <li>{props.title}</li>
         <li>Home</li>
         <li>About</li>
         <li>Features</li>
