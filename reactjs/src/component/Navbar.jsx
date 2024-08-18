6156bf94a7af99b78cbcff738a4ae5f7e27675bb
Faizyzaidi <faizyzaidi80@gmail.com>
2024-08-18 06:51:20 +0000

    CH

---
+++ reactjs/src/component/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Navbar.css'
+function Navbar() {
+  return (
+    <div className ="navbar">
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Features</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
